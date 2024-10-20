42b94566d115f3a47d1ce01e230ddc645691ea97
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-10-20 21:45:41 +0200

    chore: remove debug logs

--- dswsc/schedules.py
+++ dswsc/schedules.py
@@ -44,8 +44,6 @@ def get_schedule_ical(group_id: str, start_date: str, end_date: str) -> str:
         # Load schedules page to aquire session cookie
         response = session.get(schedules_url)
 
-        print(response.text)
-
         session_cookie = session.cookies.get_dict().get('ASP.NET_SessionId')
         if session_cookie:
             print(f'Aquired session: {session_cookie}')
