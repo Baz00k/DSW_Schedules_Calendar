0bc01ed5c65497a5d27039e7e80ebfb06743a2ef
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-11-07 15:15:48 +0100

    fix: handle empty schedule case before syncing with Google Calendar

--- dswsc/__main__.py
+++ dswsc/__main__.py
@@ -11,9 +11,13 @@ def main():
     end_date = (datetime.now() + timedelta(weeks=settings.date_range))
 
     schedule_ical = get_schedule_ical(settings.group_id, start_date.strftime(settings.date_format), end_date.strftime(settings.date_format))
-    clear_event_from_date(settings.google_calendar_id, start_date)
-    
     events = parse_ical_to_events(schedule_ical)
+    
+    if len(events) == 0:
+        print('Empty schedule, nothing to sync!')
+        return
+    
+    clear_event_from_date(settings.google_calendar_id, start_date)
     add_events_to_google_calendar(events, settings.google_calendar_id)
 
     print('Sync complete!')
